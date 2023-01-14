cc0647d2030762a6ff3ff9e75e1e5b03eba2729b
Leha <managestudylexa@gmail.com>
2023-01-14 14:26:06 +0300

    feat: added users visualisation
    добавил отображение юзеров в таблицу на index.html

--- App/views.py
+++ App/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+import datetime
 
+from django.shortcuts import render
+from App import models
+
+def add_to_users(name, surname, email):
+    item = models.Users(name=name, surname=surname, email=email, date=datetime.date.today())
+    item.save()
+
+def get_users():
+    return models.Users.objects.all()
 
 def profil(request):
     context = dict()
@@ -7,7 +16,11 @@ def profil(request):
 
 
 def index(request):
+    add_to_users('Leha', 'Mirkin', 'lehamail@gmail.com')
+    data = get_users()
     context = dict()
-    context['massage'] = 'Пока пусто'
+    context['message'] = 'Пока пусто'
+    context['data'] = data
+
 
     return render(request, 'index.html', context=context)
