72d6e82377b40195921e5827dce62abefc06f0b3
Sergey Baskakov <>
2023-01-15 19:49:02 +0300

    Added functions, Type: feat
    Добавлены функции create_user_answer и read_user_answer в App/tables_classes/Answer_Users.py

--- App/tables_classes/Answer_Users.py
+++ App/tables_classes/Answer_Users.py
@@ -2,4 +2,9 @@ from App import models
 import datetime
 
 class Answer_Users:
-    pass
+    def create_user_answer(self, description, voting_id):
+        item = Answer_Users(description=description, voting_id=voting_id)
+        item.save()
+
+    def read_user_answer(self):
+        return Answer_Users.objects.all()
