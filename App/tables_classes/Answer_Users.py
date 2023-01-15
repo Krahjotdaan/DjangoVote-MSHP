d086d5f1a34518087283123fe201836658b33745
Sergey Baskakov <>
2023-01-15 20:07:13 +0300

    Update functions, Type: fix
    Добавлены исправления функций

--- App/tables_classes/Answer_Users.py
+++ App/tables_classes/Answer_Users.py
@@ -2,9 +2,11 @@ from App import models
 import datetime
 
 class Answer_Users:
-    def create_user_answer(self, description, voting_id):
+    @staticmethod
+    def create(description, voting_id):
         item = Answer_Users(description=description, voting_id=voting_id)
         item.save()
 
-    def read_user_answer(self):
+    @staticmethod
+    def read():
         return Answer_Users.objects.all()
