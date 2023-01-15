6df4e673910ef4df8ac6b0d60fdaf4236a308def
Дындарь Маргарита <rita.dyndar@mail.ru>
2023-01-15 20:26:27 +0300

    Added functions, Type: feat
    Добавила фукнции, отвечающие за обновление и удаление записей в users

--- App/tables_classes/Users.py
+++ App/tables_classes/Users.py
@@ -6,6 +6,25 @@ class Users:
     def add(name, surname, email):
         item = models.Users(name=name, surname=surname, email=email, date=datetime.date.today())
         item.save()
+
+
     @staticmethod
     def get():
         return models.Users.objects.all()
+
+
+    @staticmethod
+    def update():
+        user_id = models.Users.id
+        users = models.Users.objects.filter(id=user_id)
+        users.name = models.Users.name
+        users.surname = models.Users.surname
+        users.email = models.Users.email
+        users.save()
+
+
+    @staticmethod
+    def delete():
+        user_id = models.Users.id
+        users = models.Users.objects.filter(id=user_id)
+        users.delete()
