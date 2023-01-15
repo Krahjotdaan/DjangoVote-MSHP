ecd4e191cfc35578e94edc6a39b75c456b49d88c
Ivan <Ivan@mail.ru>
2023-01-15 20:19:36 +0300

    feat: Answer_Choice add, get

--- App/tables_classes/Answer_Choise.py
+++ App/tables_classes/Answer_Choise.py
@@ -8,7 +8,6 @@ class Answer_Choise:
         item = models.Answer_Choise(id=id, description=description, date=datetime.date.today(), voting_id=voting_id)
         item.save()
 
-
     @staticmethod
     def get():
         return models.Answer_Choise.objects.all()
