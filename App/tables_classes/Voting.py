5005fe0451b20f3da2cb1cafd2297d419b5933a5
Ostapenko Artem <abc@mail.ru>
2023-01-15 19:41:16 +0300

    Added Voting.add and Voting.get, type: feat
    Добавлены функции создания и чтения голосования

--- App/tables_classes/Voting.py
+++ App/tables_classes/Voting.py
@@ -2,4 +2,12 @@ from App import models
 import datetime
 
 class Voting:
-    pass
+    @staticmethod
+    def add(title, description, author):
+        item = models.Voting(title=title, description=description, date=datetime.date.today(), author=author)
+        item.save()
+
+
+    @staticmethod
+    def get():
+        return models.Voting.objects.all()
