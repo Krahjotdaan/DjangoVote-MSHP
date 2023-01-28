5b886f26aa81a73307d5be5742f2ccdc4e55a7c2
Andrew Znaev <Karatelhdr@mail.ru>
2023-01-28 16:21:13 +0300

    One bug fix, Type: fix
    Убрал ненужные import, которые мешали запуску

--- App/views.py
+++ App/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.detail import DetailView
-from App.models import Profile
+from django.shortcuts import render
 
 
 def profil(request):
