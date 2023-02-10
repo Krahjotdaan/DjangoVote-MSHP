73bb146ec04207d8e54e23587a3db9a67e521d7c
Ostapenko Artem <abc@mail.ru>
2023-02-10 18:27:05 +0300

    Type: feat; added messages
    Добавлены всплывающие сообщения

--- App/forms.py
+++ App/forms.py
@@ -12,12 +12,14 @@ class VotingForm2Variants(forms.Form):
     variant1 = forms.CharField(label='вариант ответa 1', max_length=300, required=True)
     variant2 = forms.CharField(label='вариант ответa 2', max_length=300, required=True)
 
+
 class VotingForm3Variants(forms.Form):
     title = forms.CharField(label='Название голосования', max_length=50, required=True)
     description = forms.CharField(label='Описание голосования', max_length=100, required=True)
     variant1 = forms.CharField(label='вариант ответa 1', max_length=300, required=True)
     variant2 = forms.CharField(label='вариант ответa 2', max_length=300, required=True)
     variant3 = forms.CharField(label='вариант ответa 3', max_length=300, required=True)
+
 
 class VotingForm4Variants(forms.Form):
     title = forms.CharField(label='Название голосования', max_length=50, required=True)
