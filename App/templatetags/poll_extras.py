ecfb73a0d04d9bb46c5f33ecac4c0640a41ecae3
Leha <managestudylexa@gmail.com>
2023-02-22 09:51:46 +0300

    build: added counter of voices

--- App/templatetags/poll_extras.py
+++ App/templatetags/poll_extras.py
@@ -3,7 +3,7 @@ from django import setup
 
 setup()
 
-from App.models import VotedVoting
+from App.models import VotedVoting, VoteFact
 
 register = template.Library()
 
@@ -13,3 +13,7 @@ def is_voted(user, voting):
         return True
     else:
         return False
+
+@register.simple_tag
+def count_voices(variant):
+    return len(VoteFact.objects.filter(variant=variant))
